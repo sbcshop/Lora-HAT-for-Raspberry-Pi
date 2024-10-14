e9d6075a0a1d4420544fe85bbee468cda3b19cc5
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2024-10-14 12:49:28 +0530

    Update lora_transmitter.py

--- lora_transmitter.py
+++ lora_transmitter.py
@@ -2,8 +2,13 @@
 import time
 import serial
 
-lora = serial.Serial(port='/dev/ttyS0',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
+# Uncomment suitable line for your Pi and comment other which is not required 
+'''For Raspberry Pi 4 & 3'''
+lora = serial.Serial(port = '/dev/ttyS0', baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1)
 
+'''For Raspberry Pi 5'''
+#lora = serial.Serial(port = '/dev/ttyAMA0', baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1)
+ 
 while True:
     n = input("Enter The Message = ")#input the string
     b = bytes(n,'utf-8')#convert string into bytes
