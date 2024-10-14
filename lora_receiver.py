ab0ff84ca6a5735f9e95cbc7a743220492f5d23e
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2024-10-14 12:49:08 +0530

    Update lora_receiver.py

--- lora_receiver.py
+++ lora_receiver.py
@@ -2,7 +2,13 @@
 import serial
 import time
 
+# Uncomment suitable line for your Pi and comment other which is not required 
+'''For Raspberry Pi 4 & 3'''
 lora = serial.Serial(port = '/dev/ttyS0', baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1)
+
+'''For Raspberry Pi 5'''
+#lora = serial.Serial(port = '/dev/ttyAMA0', baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 1)
+ 
 
 while True:
     data_read = lora.readline()#read data from other lora
